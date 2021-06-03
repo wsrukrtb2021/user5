a8fafe96b4060c1060e6d326c966435f7da2224c
unknown <lorp.lop@yandex.ru>
2021-06-03 11:48:34 +0300

    add_new_project

---
+++ Lopushock/Class1/Connect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lopushock.Class1
+{
+    class Connect
+    {
+        public static string String { get; } = "Data Source=WSR;Initial Catalog=user5;uid=user5;pwd=passuser5;";
+    }
+}
---
+++ Lopushock/Usercontrol1/PaperUC.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Data.SqlClient;
+using Lopushock.Class1;
+using Lopushock.Usercontrol1;
+
+
+namespace Lopushock.Usercontrol1
+{
+    /// <summary>
+    /// Логика взаимодействия для PaperUC.xaml
+    /// </summary>
+    public partial class PaperUC : UserControl
+    {
+        public PaperUC()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
